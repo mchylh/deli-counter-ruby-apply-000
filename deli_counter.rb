9af8ff39bb99415b7c285e2f8e886697e89b1e4e
Michaylah Mam <mchylh@gmail.com>
2020-05-07 23:03:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,14 @@
+#line method: shows everyone's current place in line
+katz_deli = []
 
+def line(x)
+  line_array = []
+  if x.length == 0
+    puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
