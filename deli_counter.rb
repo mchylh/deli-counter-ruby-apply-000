214ddac670dafd2feccc1c9cf4efed1cf2d07831
Michaylah Mam <mchylh@gmail.com>
2020-05-07 23:05:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,3 @@
-#line method: shows everyone's current place in line
 katz_deli = []
 
 def line(x)
@@ -10,5 +9,17 @@ def line(x)
       line_array.push("#{index}. #{name}")
     end
     puts "The line is currently: #{line_array.join(" ")}"
+  end
+end
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
+def now_serving(array)
+  if array.empty?
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
   end
 end
